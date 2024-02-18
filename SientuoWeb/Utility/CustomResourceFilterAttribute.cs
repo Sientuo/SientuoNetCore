c7391132f91d3ce495fac39f540ddefe5d485885
Sientuo <58808100+Sientuo@users.noreply.github.com>
2024-02-18 21:14:04 +0800

    core 特性添加项目文件。

---
+++ SientuoWeb/Utility/CustomResourceFilterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using SientuoComm.Helper;
+
+namespace SientuoWeb.Utility
+{
+    public class CustomResourceFilterAttribute : Attribute, IResourceFilter
+    {
+        void IResourceFilter.OnResourceExecuted(ResourceExecutedContext context)
+        {
+            LogHelper.WriteInfo("资源后发生");
+        }
+
+        void IResourceFilter.OnResourceExecuting(ResourceExecutingContext context)
+        {
+            LogHelper.WriteInfo("资源前发生");
+        }
+    }
+}
